d49d07020b48b9fdc3c87d74c49e319536efe028
stitch099 <122793174+stitch099@users.noreply.github.com>
2023-01-21 00:50:47 +0200

    as is

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import axios from "axios";
 const axios = require('axios');
 
+let domainNameUrl = 'https://alter.com/blog/top-domain-name-sales'
+const main = async () => {
+    axios.get('https://alter.com/blog/top-domain-name-sales').then(response=>{
+        // console.log(response.data)
+        let htmlText =  response.data;
+        // TODO parse logic 
+
+    });
+
+    //  try {
+    //   let response = await fetch("https://alter.com/blog/top-domain-name-sales", requestOptions) 
+    //   let result = await response.json()
+    //   console.log(`result.value is ${result.USD.last}`)
+    // } catch (error) {
+    //   console.log('error', error)
+    // }
+};
+
+main();
